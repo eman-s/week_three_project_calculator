6e59a742c5f887b7162dd71e94ce76c1ab4713cd
Emmanuel Salicrup <emmanuel_salicrup@live.com>
2017-07-19 21:30:31 -0400

    My name is Ozymandias, king of kings: Look on my works, ye Mighty, and despair

--- main.js
+++ main.js
@@ -8,12 +8,24 @@ let btnParent = document.querySelector(".calculator")
 
 
   btnParent.addEventListener('click', function(e){
-    var magicalBox = []
-    let buttonValue = e.target.value;
+
+
+    if(e.target.value !== "="){
+      var magicalBox = []
+      let buttonValue = e.target.value;
+
+      display.value += buttonValue;
+      magicalBox.push(display.value);
+      console.log(magicalBox);
+      let magicalString = magicalBox.toString();
+      console.log(magicalString);
+      let magicalSolution = eval(magicalString);
+      console.log(magicalSolution);
+    }
     
-    display.value += buttonValue;
-    magicalBox.push(buttonValue);
-    console.log(magicalBox);
+
+
+
 
 
 
