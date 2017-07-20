d9b888b1675a5e98cd5fddcb06c9c168138675e9
Emmanuel Salicrup <emmanuel_salicrup@live.com>
2017-07-19 22:44:11 -0400

    (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 let display = document.querySelector(".display");
-
 let btnParent = document.querySelector(".calculator")
 let equals = document.querySelector(".equals")
-
+let clear = document.querySelector(".clear")
 
 
   btnParent.addEventListener('click', function(e){
-
         var magicalBox = []
-
         let buttonValue = e.target.value;
 
           display.value += buttonValue;
@@ -17,13 +14,18 @@ let equals = document.querySelector(".equals")
           let magicalString = magicalBox.toString();
           console.log(magicalString);
 
-
           function getSolution(){
           let magicalSolution = eval(magicalString);
           console.log(magicalSolution);
           display.value = magicalSolution;
           }
+          function clearScreen(){
+              display.value = "";
+          }
+
           equals.addEventListener("click", getSolution);
+          clear.addEventListener("click", clearScreen);
+
 
 
 
